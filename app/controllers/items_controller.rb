378e18cb3c4724642ca4c18bf7637abdf7fc9ed2
yuri <bebejuli7339@gmail.com>
2019-02-06 06:38:53 +0900

    商品出品画面のMVCの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+    @item.itemimages.build
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'test#index'
-  resources :items, only: :index
+  resources :items, only: [:index, :new]
 end
