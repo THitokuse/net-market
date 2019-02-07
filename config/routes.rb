4ae6d26dfbf49387ae06e0f440812d39b6715c0a
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-08 01:25:20 +0900

    deviseに基づくusersのルーティングを生成
    ・jp/signup
    →登録方法選択画面
    ・jp/signup/registration
    →基本情報登録画面
    ・jp/signup/sms_confirmation
    →SMS生成用電話番号登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'jp/signup' => 'users/registrations#index'
+    get 'jp/signup/registration' => 'users/registrations#new'
+    get 'jp/signup/sms_confirmation' => 'users/registrations#sms_confirmation'
+  end
   root 'test#index'
   resources :items, only: :index
 end
