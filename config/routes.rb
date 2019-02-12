200b616c622f1e7263f132f47fa9099bb2f6bcf0
yuri <bebejuli7339@gmail.com>
2019-02-13 02:48:57 +0900

    商品購入確認ページのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'test#index'
-  resources :items, only: [:index, :new, :show] do
+  resources :items, only: [:index, :new, :show, :purchase_concern] do
     resources :comments, only: [:create]
+      member do
+        get "purchase_concern"
+      end
   end
   get 'logout' => 'mypages#logout'
 
