577b0e96d7e99c635381d739040403a805c3712a
比嘉将吾 <handball.7@i.softbak.jp>
2019-02-12 20:06:43 +0900

    タブ機能の実装

---
+++ app/assets/javascripts/tab.js
@@ -0,0 +1,12 @@
+$(window).on("load", function() {
+  $("#mypage-top_left, #mypage-top_right").on("click", function() {
+    $(this).addClass("active");
+    $(".mypage-tab-notification_top").hide();
+    $("." + this.id).show();
+  });
+  $("#mypage-bottom_left, #mypage-bottom_right").on("click", function() {
+    $(this).addClass("active");
+    $(".mypage-tab-notification_bottom").hide();
+    $("." + this.id).show();
+  });
+});
