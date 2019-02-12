ddfc848029abaf2958de891d2feba8961af967bc
比嘉将吾 <handball.7@i.softbak.jp>
2019-02-12 21:37:33 +0900

    rake db:migrateの結果

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,24 @@ ActiveRecord::Schema.define(version: 2019_02_09_092251) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "deliveryburdens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deliverydates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deliverymethods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id"
     t.string "image", null: false
