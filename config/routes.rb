85efed5a400d20bd1b91ff022bb0b38a2c7c3f5c
yuri <bebejuli7339@gmail.com>
2019-02-13 03:17:01 +0900

    コンフリクトの修正 ver2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'test#index'
-  resources :items, only: [:index, :new, :show, :purchase_concern] do
-=======
   root 'items#index'
   resources :items, only: [:index, :new, :show] do
->>>>>>> master
     resources :comments, only: [:create]
       member do
         get "purchase_concern"
