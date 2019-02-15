3d0ce6b08a45ea1e00923231020d47b9ec3f9c80
yuri <bebejuli7339@gmail.com>
2019-02-16 03:03:22 +0900

    ビュー(仮)とユーザーテーブルのカラムにバリデーションの設定
    ログイン画面のビューはメンバーが作っている為、今は簡易ビューで補っています。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :tel, presence: true
+  validates :prefecture_cede, presence: true
+  validates :zip, presence: true
+  validates :city_name, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
 end
