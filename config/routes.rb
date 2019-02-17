1333e2a865b63bc50a85058e9ce587c64571bc2e
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-18 01:14:48 +0900

    ログイン画面実装
    ログイン画面のview実装
    ・ルーティング
    jp/loginに指定
    
    ・未達箇所
    Google承認
    Facebook承認
    →omniauthを実装する予定

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'jp/signup' => 'users/registrations#index'
     get 'jp/signup/registration' => 'users/registrations#new'
+    post 'jp/signup/registration' => 'users/registrations#create'
     get 'jp/signup/sms_confirmation' => 'users/registrations#sms_confirmation'
+
+    get 'jp/login' => 'users/sessions#new'
   end
-  root 'test#index'
+  root 'items#index'
   resources :items, only: :index
 end
