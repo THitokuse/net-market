76439589aac3c67e6a671d25065db574f9ca8942
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-25 00:47:45 +0900

    Userモデルをjp_prefecutureの実装
      def prefecture_name
        JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
      end
    
      def prefecture_name=(prefecture_name)
        self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
      end
    を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
 
   include JpPrefecture
   jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
   with_options presence: true do
     validates :nickname
     validates :first_name
