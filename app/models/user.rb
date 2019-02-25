1712641d1e0f8593c40c3db5dbe4b9c2b0981917
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-26 00:53:00 +0900

    prefecture_codeの実装
    jp_prefectureの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   include JpPrefecture
   jp_prefecture :prefecture_code
 
-  def prefecture_name
-    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
-  end
+  # def prefecture_name
+  #   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  # end
 
-  def prefecture_name=(prefecture_name)
-    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
-  end
+  # def prefecture_name=(prefecture_name)
+  #   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  # end
 
   with_options presence: true do
     validates :nickname
