016cca298517768ae75430686531bce7434c5f83
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-27 02:19:57 +0900

    deviseにomniauth設定を記載
    googleとfacebookで登録できるようにする。
    GOOGLEとFACEBOOKのキーとシークレットキーの環境変数呼び出し

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,7 +262,18 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  require 'devise/orm/active_record'
+  config.omniauth :google_oauth2,
+                  ENV['GOOGLE_APP_ID'], # 環境変数に先ほど控えたクライアントIDを入れておく
+                  ENV['GOOGLE_APP_SECRET'], # 環境変数に先ほど控えたシークレットを入れておく
+                  name: :google,
+                  scope: %w(email)
 
+  config.omniauth :facebook,
+                  ENV['FACEBOOK_KEY'],
+                  ENV['FACEBOOK_SECRET'],
+                  name: :facebook,
+                  scope: %w(email)
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
