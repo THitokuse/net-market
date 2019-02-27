093cc2f22e3c4c5bac5ac96dcabc6a245d426cc9
YuriKosaki <bebejuli7339@gmail.com>
2019-02-27 17:56:17 +0900

    delivery_method外部キーのコメントアウト

--- db/migrate/20190216161242_remove_delivery_from_item.rb
+++ db/migrate/20190216161242_remove_delivery_from_item.rb
@@ -2,6 +2,6 @@ class RemoveDeliveryFromItem < ActiveRecord::Migration[5.2]
   def change
     remove_reference :items, :deliveryburden, null: false
     remove_reference :items, :deliverydate, null: false
-    remove_reference :items, :deliverymethod, null: false
+#     remove_reference :items, :deliverymethod, null: false
   end
 end
