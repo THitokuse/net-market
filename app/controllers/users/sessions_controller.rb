6cf0dedab2c83448d492bd05c1716d730102721e
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-02-28 21:19:22 +0900

    不要なコメントを削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,16 +13,4 @@ class Users::SessionsController < Devise::SessionsController
   def create
     super
   end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
