8fa5e1755a1336cb633e9f730d6e35d6fb6af85d
比嘉将吾 <handball.7@i.softbak.jp>
2019-03-04 04:00:52 +0900

    コントローラーの実装

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,22 +1,41 @@
 class CreditsController < ApplicationController
   protect_from_forgery except: :create
+
   def index
+    @credit_card = Credit.find_by(user_id: current_user.id)
   end
 
   def new
-    @credit = Credit.new
+    get_new_credit_months_years
   end
 
   def create
-    # binding.pry
-    # @credit = Credit.create(credit_params)
-    # Payjp.api_key = Rails.application.credentials.payjp_secret_key
-    # Payjp::Charge.create(currency: 'jpy', amount: 1000, card: params['payjp-token'])
-    # redirect_to root_path, notice: "支払いが完了しました"
+    @credit_card = Credit.new(credit_params)
+    if @credit_card.save
+      render :index
+    else
+      get_new_credit_months_years
+      render :new
+    end
+  end
+
+  def destroy
+    credit_card = Credit.find(params[:id])
+    if credit_card.user_id == current_user.id
+      credit_card.destroy
+    end
+    redirect_to credits_path
   end
 
   private
+
   def credit_params
-    params.permit(:number,:cvc,:exp_month, :exp_year).merge(user_id: current_user.id)
+    params.require(:credit).permit(:authorization_code, :security_code, :month, :year).merge(user_id: current_user.id)
+  end
+
+  def get_new_credit_months_years
+    @credit_card = Credit.new
+    @months = Credit.get_months
+    @years = Credit.get_years
   end
 end
