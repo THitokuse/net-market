af96a8506cdf5ea48f138e7d4a058a171c5cb4e8
yuri <bebejuli7339@gmail.com>
2019-04-14 00:53:22 +0900

    discard_deliveryburden

--- db/migrate/20190206072206_add_item_to_delivery_contents.rb
+++
@@ -1,6 +0,0 @@
-class AddItemToDeliveryContents < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :items, :deliveryburden, null: false
-    add_reference :items, :deliverydate, null: false
-  end
-end
