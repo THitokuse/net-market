d28edff23d8150d5056e9cd2fa754cf6c4c55ebe
yuri <bebejuli7339@gmail.com>
2019-04-18 02:03:36 +0900

    トップページをレディースとメンズに分ける

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
 
   before_action :set_locale
+  before_action :set_items
+  before_action :one_item_setting, only: [:show, :edit, :update, :destroy, :purchase_concern, :sell_item]
   before_action :move_to_index, except: [:index, :show]
   protect_from_forgery except: :update
   before_action :item_setting, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show, :purchase_concern]
 
   def index
-    @items = Item.includes(:item_images).order("created_at DESC").limit(4)
+    @items = @selling_items.limit(4)
+    @ladies_items = @items.where('upper_category_id = ?', 1)
+    @mens_items = @items.where('upper_category_id = ?', 2)
   end
 
   def new
@@ -29,22 +33,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @comment = Comment.new
-    @items = Item.all.where(user_id: @item.user.id).where.not(id: @item.id)
-    @brand_items = Item.all.where(brand_id: @item.brand.id).where.not(id: @item.id)
+    @items = @selling_items.where(user_id: @item.user.id).where.not(id: @item.id)
+    @brand_items = @selling_items.where(brand_id: @item.brand.id).where.not(id: @item.id)
   end
 
   def edit
-    @item = Item.find(params[:id])
-
     @item.item_images.each do |img|
       @image = img.image
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
       @item.update(item_params)
       redirect_to item_path(@item)
@@ -54,7 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to item_listing_mypages_path, notice: '商品を削除しました'
@@ -62,12 +61,10 @@ class ItemsController < ApplicationController
   end
 
   def purchase_concern
-    @item = Item.find(params[:id])
     @credit_card = Credit.find_by(user_id: current_user.id)
   end
 
   def sell_item
-    @item = Item.find(params[:id])
   end
 
   private
@@ -76,6 +73,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :prefecture_code, :content, :upper_category_id, :middle_category_id, :lower_category_id, :size_id, :brand_id, :delivery_burden_id, :delivery_date_id, :delivery_method_id, :status_id, :condition_id, :purchase_status, item_images_attributes: [:id, :image]).merge(user_id: current_user.id)
   end
 
+  def one_item_setting
+    @item = Item.find(params[:id])
+  end
 
 
   def item_setting
