1e90ee708d414a6ca20413c61b76513df428c3d7
yuri <bebejuli7339@gmail.com>
2019-04-18 02:03:51 +0900

    検索機能の修正

--- app/controllers/multi_searches_controller.rb
+++ app/controllers/multi_searches_controller.rb
@@ -1,8 +1,9 @@
 class MultiSearchesController < ApplicationController
   before_action :item_setting, only: :index
+  before_action :set_items, only: :index
 
   def index
-    @new_items = Item.order("created_at DESC").limit(20)
+    @new_items = @selling_items.limit(20)
     @search = Item.ransack(params[:q])
     @items = @search.result.includes(:upper_category).page(params[:page]).per(20)
   end
@@ -10,7 +11,7 @@ class MultiSearchesController < ApplicationController
   private
     def item_setting
     @upper_categories = UpperCategory.all.includes([middle_categories: :lower_categories])
-    @sizes = Size.all.where(size_type_id: 1)
+    @sizes = Size.all.where('size_type_id = ?', 1)
     @delivery_burdens = DeliveryBurden.all
     @conditions = Condition.all
     @statuses = Status.all
--- app/controllers/simple_searches_controller.rb
+++ app/controllers/simple_searches_controller.rb
@@ -1,9 +1,10 @@
 class SimpleSearchesController < ApplicationController
   before_action :item_setting, only: :index
+    before_action :set_items, only: :index
 
   def index
     @keyword = params[:keyword]
-    @items = Item.where('name LIKE(?) OR content  LIKE(?)',"%#{params[:keyword]}%","%#{params[:keyword]}%").page(params[:page]).per(20)
+    @items = @selling_items.where('name LIKE(?) OR content  LIKE(?)',"%#{params[:keyword]}%","%#{params[:keyword]}%").page(params[:page]).per(20)
     @search = Item.ransack(params[:q])
     @new_items = Item.order("created_at DESC").limit(20)
   end
@@ -11,7 +12,7 @@ class SimpleSearchesController < ApplicationController
   private
     def item_setting
     @upper_categories = UpperCategory.all.includes([middle_categories: :lower_categories])
-    @sizes = Size.all.where(size_type_id: 1)
+    @sizes = Size.where('size_type_id = ?', 1)
     @delivery_burdens = DeliveryBurden.all
     @conditions = Condition.all
     @statuses = Status.all
